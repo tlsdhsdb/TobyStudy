941ba527bfa576afaa5c3c86de9430f020bc8b9a
onyoo <dhsdb02@gmail.com>
2024-07-05 01:19:39 +0900

    초기 커밋

---
+++ src/main/java/com/example/toby/TobyApplication.java
@@ -0,0 +1,13 @@
+package com.example.toby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobyApplication.class, args);
+	}
+
+}
